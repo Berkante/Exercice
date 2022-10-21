2ebc3996e1f30d8dedb52a83f6f112ae42b92097
Berkante Ayoub <116183900+Berkante@users.noreply.github.com>
2022-10-21 10:58:58 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -1,9 +1,8 @@
+#---------------------------------------------------------------------
 #Écrire un algorithme prenant 2 ou 3 nombres 
 #Effectuer et retourner la somme des trois nombres, la soustraction des deux derniers nombres et la multiplication des deux premiers. 
 #Si la troisième valeur n'est pas entrée assignez-lui la valeur 7.
-
-
-
+#---------------------------------------------------------------------
 
 def fonctions (a,b ,c):
     multiplication= a*c
