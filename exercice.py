d8e240140d9787850a24d09f1bcd4783838abb11
Berkante Ayoub <Berkanteayoub@gmail.com>
2022-10-21 10:57:04 -0400

    Exercice -1
    Simple algorithme:
    multiplication
    somme
    soustraction

---
+++ exercice.py
@@ -0,0 +1,18 @@
+#Écrire un algorithme prenant 2 ou 3 nombres 
+#Effectuer et retourner la somme des trois nombres, la soustraction des deux derniers nombres et la multiplication des deux premiers. 
+#Si la troisième valeur n'est pas entrée assignez-lui la valeur 7.
+
+
+
+
+def fonctions (a,b ,c):
+    multiplication= a*c
+    somme = a + b + c
+    soustraction= b - c
+    return multiplication, somme, soustraction
+print(fonctions(5, 6, 7))
+print(somme)
+print(soustractio)
+print(multiplication)
+
+
